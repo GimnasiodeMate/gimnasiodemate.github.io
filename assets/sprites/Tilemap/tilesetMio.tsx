a94b7df2de052b8c8c73aff0094c9b485e49d04e
GimnasiodeMate <83723690+GimnasiodeMate@users.noreply.github.com>
2021-06-15 22:43:24 -0600

    Delete assets directory

--- assets/sprites/Tilemap/tilesetMio.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="tilesetMio" tilewidth="64" tileheight="64" tilecount="204" columns="17">
- <image source="tilesnuevas.png" width="1088" height="768"/>
-</tileset>
